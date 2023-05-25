2335238a5c74917f4feee0f3f8ba87abc73b6b10
datnguyen <nguyenhoangdat9893@gmail.com>
2023-05-25 11:24:44 +0700

    update diagram and Security Group

--- modules/sg/main.tf
+++ modules/sg/main.tf
@@ -31,12 +31,43 @@ resource "aws_security_group" "alb_security_group" {
     Name = "alb security group"
   }
 }
+# create security group for Jump Box
+resource "aws_security_group" "ssh_security_group" {
+  name        = "SSH Access"
+  description = "Enable SSH access on Port 22"
+  vpc_id      = var.vpc.id
 
-# create security group for ec2
+  ingress {
+    description      = "SSH access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = -1
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  tags   = {
+    Name = "SSH Security Group"
+  }
+}
+# create security group for private app ec2
 resource "aws_security_group" "ec2_security_group" {
   name        = "ec2 security group"
-  description = "enable http/https access on port 80/443 via alb sg"
+  description = "enable http/https access on port 80/443 via ALB SG and SSH on port 22 via SSH SG"
   vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "SSH access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    security_groups  = [aws_security_group.ssh_security_group.id]
+  }
 
   ingress {
     description      = "http access"
@@ -62,6 +93,32 @@ resource "aws_security_group" "ec2_security_group" {
   }
 
   tags   = {
-    Name = "ec2 security group"
+    Name = "Database security group"
+  }
+}
+
+# create secuity group for Database 
+
+resource "aws_security_group" "database_security_group" {
+  name        = "Database security group"
+  description = "Enable PostgreSQL access on port 5432"
+  vpc_id      = var.vpc.id
+
+  ingress {
+    description      = "PostgreSQL Access"
+    from_port        = 5432
+    to_port          = 5432
+    protocol         = "tcp"
+    security_groups  = [aws_security_group.ec2_security_group.id]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = -1
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  tags   = {
+    Name = "SSH Security Group"
   }
 }
